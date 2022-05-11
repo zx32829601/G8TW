214eb2cf4bc0e93d8f7954c651d308dabe00cc0b
zx328 <zx328@DESKTOP-FT48EIU>
2022-05-11 13:58:44 +0800

    ver4

--- TWproject/src/test.java
+++ TWproject/src/test.java
@@ -1,21 +1,4 @@
 public class test{
-    public static void main(String[] args) {
-        Wristband wristband=new Wristband();
-        wristband.sensor.Set_Heartrhythm(140);  
-        System.out.println(wristband.sensor.Get_Heartrhythm());
-        System.out.println(Controller.Record_Heart_Rhythm(wristband.sensor.Get_Heartrhythm()));
-        System.out.println(Controller.Get_Message());
-        wristband.sensor.Set_Heartrhythm(90);
-        System.out.println(wristband.sensor.Get_Heartrhythm());
-        Controller.Record_Heart_Rhythm(wristband.sensor.Get_Heartrhythm());
-        SendMsg.Decrease_n(false);
-        
-        System.out.println(Controller.Get_Message());
-        wristband.blood_oxy.Set_blood_oxy(98);
-        System.out.println(wristband.blood_oxy.Get_bloodoxy());
-        wristband.gps.Set_GPS(14030.24,149592.0);
-        System.out.println(wristband.gps.Get_LatutudeGPS());
-        System.out.println(wristband.gps.Get_LongGPS());
-    }
+    
  }
  
